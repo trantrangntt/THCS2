b79c761d6a105b75c2434c7e3864be1eea87dbdb
Trang đâyy nekk <91332653+trantrangntt@users.noreply.github.com>
2021-10-19 17:57:22 +0700

    motbaidelay500ngay

--- khoang_cach_bangx.cpp
+++ khoang_cach_bangx.cpp
@@ -9,10 +9,21 @@ int main()
         int n, x;
         cin >> n >> x;
         int a[n];
+        int b[100005] = {0};
         int ktra = 0;
         for (int i = 0; i < n; i++)
+        {
             cin >> a[i];
-
+            b[a[i]] = 1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (b[a[i] - x] == 1)
+            {
+                ktra = 1;
+                break;
+            }
+        }
         if (ktra == 1)
             cout << "1";
         else
